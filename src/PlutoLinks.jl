8dd4c604d55c66f920e1e369100fd36515e6ab1c
Pangoraw <naydex.mc+github@gmail.com>
2022-02-02 21:50:10 +0000

    Deploying to gh-pages from  @ dc7f3950cf16dc5ad36e481a52fabe855421f35a 🚀

--- src/PlutoLinks.jl
+++ src/PlutoLinks.jl
@@ -1,12 +1,7 @@
 module PlutoLinks
 
-using Requires
-
 include("./notebook.jl")
-
-function __init__()
-    @require Revise="295af30f-e4ad-537b-8983-00126c2a3abe" include("./revise.jl")
-end
+include("./revise.jl")
 
 export @use_file, @use_task, @ingredients, @revise
 
