abc0951d985e29118b3a6c8666fe1cedae131c83
Paul <paul@plutojl.org>
2022-02-02 19:47:12 +0100

    add `@revise`

--- src/PlutoLinks.jl
+++ src/PlutoLinks.jl
@@ -1,7 +1,13 @@
 module PlutoLinks
 
+using Requires
+
 include("./notebook.jl")
 
-export @use_file, @use_task, @ingredients
+function __init__()
+    @require Revise="295af30f-e4ad-537b-8983-00126c2a3abe" include("./revise.jl")
+end
+
+export @use_file, @use_task, @ingredients, @revise
 
 end
