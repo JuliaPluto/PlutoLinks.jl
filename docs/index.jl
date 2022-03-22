e0af5c0ffce1bdf4179c2b005f975c048812b3ef
Paul <paul@plutojl.org>
2022-03-22 20:33:03 +0100

    fix docs gen

--- docs/index.jl
+++ docs/index.jl
@@ -1,8 +1,11 @@
 ### A Pluto.jl notebook ###
-# v0.18.1
+# v0.18.4
 
 using Markdown
 using InteractiveUtils
+
+# ╔═╡ bdf53020-635d-4cc9-8a0e-a612ca470e85
+hook_link(p) = replace(p, ".jl" => ".html");
 
 # ╔═╡ 7c852ed0-36b8-43ea-8ee8-c4bd45e9c8a9
 readme = read(joinpath(@__DIR__, "../README.md"), String) |> Markdown.parse;
@@ -12,7 +15,13 @@ hook_name(file) = file == "notebook.jl" ? "Other hooks" : "@" * replace(basename
 
 # ╔═╡ 11c144eb-60b4-4a64-8077-29e97abedce8
 docs = let
-	files = map(f -> " - [`$(hook_name(f))`](./$f)", filter(!=("index.jl") ∘ basename, readdir("./"))) |> s -> join(s, "\n") |> Markdown.parse
+	files = map(
+		f -> " - [`$(hook_name(f))`](./$(hook_link(f)))",
+		filter(
+			name -> basename(name) != "index.jl" && endswith(name, ".jl"),
+			readdir("./")
+		)
+	) |> s -> join(s, "\n") |> Markdown.parse
 
 	md"""
 	#### Documentation
@@ -49,7 +58,7 @@ PLUTO_PROJECT_TOML_CONTENTS = """
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.1"
+julia_version = "1.7.2"
 manifest_format = "2.0"
 
 [deps]
@@ -58,6 +67,7 @@ manifest_format = "2.0"
 # ╔═╡ Cell order:
 # ╟─2175e675-df15-4934-b410-d0def1decf04
 # ╟─11c144eb-60b4-4a64-8077-29e97abedce8
+# ╟─bdf53020-635d-4cc9-8a0e-a612ca470e85
 # ╟─3e715447-312c-4c5f-85a3-96abbb91fc0c
 # ╟─7c852ed0-36b8-43ea-8ee8-c4bd45e9c8a9
 # ╟─d974bcaa-935e-4a8c-bfc8-827034e79efe
