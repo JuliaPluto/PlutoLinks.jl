2a175c44620c3f90c1113ba7ec4ab928546e07af
Paul <paul@plutojl.org>
2022-11-11 13:47:14 +0100

    Add `@use_process` and `@use_process_output`
    Following https://github.com/fonsp/Pluto.jl/pull/2249

--- src/PlutoLinks.jl
+++ src/PlutoLinks.jl
@@ -3,7 +3,8 @@ module PlutoLinks
 include("./notebook.jl")
 include("./revise.jl")
 include("./debounce.jl")
+include("./process.jl")
 
-export @use_debounce, @use_file, @use_task, @ingredients, @revise
+export @use_debounce, @use_file, @use_task, @ingredients, @revise, @use_process, @use_process_output
 
 end
